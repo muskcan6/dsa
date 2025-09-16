cccf76c87fef31766192e6cf8025961d74372a8d
muskcan6 <muskcan6@gmail.com>
2025-09-16 09:31:01 +0530

    Time: 1 ms (52.99%), Space: 40.9 MB (60.41%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -5,6 +5,6 @@ class Solution {
             if (n % 2 == 0) n = n / 2; else if (n % 3 == 0) n = n / 3; else if (n % 5 == 0) n = n / 5; else return false;
         }
 
-        return n == 1;
+        return n == 1; // handles negatives
     }
 }
