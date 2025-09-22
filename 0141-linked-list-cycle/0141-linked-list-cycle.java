f8b463c5afefac73d62f80a9f507dff66ec871d0
muskcan6 <muskcan6@gmail.com>
2025-09-23 03:23:09 +0530

    Time: 0 ms (100%), Space: 44.5 MB (62.66%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        // brute force -- add elements to array/list/set check for repeating element, if repeats its a cycle else nope -- takes extra space
+
+        // hare turtle - slow fast - floyds cycle detection algo- o(n) time and constant space
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
