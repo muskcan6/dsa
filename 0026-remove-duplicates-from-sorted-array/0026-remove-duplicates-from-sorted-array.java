5bb3dfb5b063daa197c8acd59cb60805684fec8d
muskcan6 <muskcan6@gmail.com>
2025-09-23 02:38:03 +0530

    Time: 1 ms (59.2%), Space: 45.1 MB (13.03%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int removeDuplicates(int[] nums) {
+        // brute force -- use additional space, remove duplicates takes extra space
+
+        // use 2 pointers, read and write pointer, tc = o(n), sc = o(1)
+
+        int read = 0, write = 0, n = nums.length;
+
+        while (read < n) {
+            int temp = read;
+            while (temp < n && nums[temp] == nums[read]) {
+                temp += 1;
+            }
+
+            nums[write++] = nums[read];
+            read = temp;
+        }
+
+        return write;
+    }
+}
