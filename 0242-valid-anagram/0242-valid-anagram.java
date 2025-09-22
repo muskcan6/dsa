82ed961d62332bc880b7cf3115367aba2d4d5ce5
muskcan6 <muskcan6@gmail.com>
2025-09-23 02:48:53 +0530

    Time: 5 ms (55.1%), Space: 42.7 MB (97.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,15 +3,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         //sort all chars and check nlogn for sort and n for check so nlogn
 
-        // use int arr, and then do array check tc = sc = o(1)
+        // use int arr, and then do array check tc = o(n), sc = o(256) thats constant
 
         if (s.length() != t.length()) return false;
 
         int a1[] = new int[26]; // make this 256 for unicode chars
         int a2[] = new int[26];
 
-        for (char c : s.toCharArray()) a1[c - 'a']++;
-        for (char c : t.toCharArray()) a2[c - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            a1[s.charAt(i) - 'a']++;
+            a2[t.charAt(i) - 'a']++;
+        }
 
         return Arrays.equals(a1, a2);
     }
