354fd4b7e0d32d7617519cf3c0ad06494f83b561
muskcan6 <muskcan6@gmail.com>
2025-09-23 01:57:51 +0530

    Time: 0 ms (100%), Space: 42.5 MB (60.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,31 +10,24 @@
  */
 class Solution {
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode head = dummy;
-        solve(dummy, list1, list2);
+    //tc = o(m+n) // basically iterating over both lists once
+    // sc (m+n) stack space for recursion -- use iteration to reduce this to constant
 
-        return head.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return solve(list1, list2);
     }
 
-    public void solve(ListNode temp, ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) return;
-
-        if (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                temp.next = new ListNode(l1.val);
-                solve(temp.next, l1.next, l2);
-            } else {
-                temp.next = new ListNode(l2.val);
-                solve(temp.next, l1, l2.next);
-            }
-        } else if (l1 != null) {
-            temp.next = new ListNode(l1.val);
-            solve(temp.next, l1.next, l2);
+    public ListNode solve(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val <= l2.val) {
+            l1.next = solve(l1.next, l2);
+            return l1;
         } else {
-            temp.next = new ListNode(l2.val);
-            solve(temp.next, l1, l2.next);
+            l2.next = solve(l1, l2.next);
+            return l2;
         }
     }
 }
