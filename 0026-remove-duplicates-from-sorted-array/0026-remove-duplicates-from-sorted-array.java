dbad9b3cd86d1a30ccdd3995b880d9c4c180290e
muskcan6 <muskcan6@gmail.com>
2025-09-23 02:41:37 +0530

    Time: 1 ms (59.2%), Space: 45.4 MB (6.75%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -5,18 +5,28 @@ class Solution {
 
         // use 2 pointers, read and write pointer, tc = o(n), sc = o(1)
 
-        int read = 0, write = 0, n = nums.length;
+        // int read = 0, write = 0, n = nums.length;
 
-        while (read < n) {
-            int temp = read;
-            while (temp < n && nums[temp] == nums[read]) {
-                temp += 1;
+        // while (read < n) {
+        //     int temp = read;
+        //     while (temp < n && nums[temp] == nums[read]) {
+        //         temp += 1;
+        //     }
+
+        //     nums[write++] = nums[read];
+        //     read = temp;
+        // }
+
+        // return write;
+
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k++] = nums[i];
             }
-
-            nums[write++] = nums[read];
-            read = temp;
         }
 
-        return write;
+        return k;
     }
 }
