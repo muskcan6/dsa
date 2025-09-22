cf900a5dbd580f0ab6881783405e95c4a1d1e9f4
muskcan6 <muskcan6@gmail.com>
2025-09-23 02:01:22 +0530

    Time: 0 ms (100%), Space: 42.4 MB (76.27%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -13,8 +13,25 @@ class Solution {
     //tc = o(m+n) // basically iterating over both lists once
     // sc (m+n) stack space for recursion -- use iteration to reduce this to constant
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return solve(list1, list2);
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // return solve(list1, list2);
+
+        ListNode dummy = new ListNode(-1), head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                dummy.next = l1;
+                l1 = l1.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+
+            dummy = dummy.next;
+        }
+
+        if (l1 != null) dummy.next = l1; else dummy.next = l2;
+
+        return head.next;
     }
 
     public ListNode solve(ListNode l1, ListNode l2) {
