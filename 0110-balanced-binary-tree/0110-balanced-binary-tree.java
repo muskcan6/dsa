8d4d05e3f3e421a7ed17a93a69cfd7d5645caa8e
muskcan6 <muskcan6@gmail.com>
2025-09-23 13:15:41 +0530

    Time: 0 ms (100%), Space: 44.8 MB (23.53%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,9 +16,31 @@
 class Solution {
 
     public boolean isBalanced(TreeNode root) {
-        // brute force -- calculate depth of left and right tree AT EVERY NODE and check if diff is greater than one if yes return false, return true
+        //top down
+        // brute force -- calculate depth of left and right tree AT EVERY NODE and check if diff is greater than one if yes return false, return true --->  tc is o(n ^ 2) in worst case of skewed tree,  best case - o(n log n) in case of balanced tree
+        //sc = recurstion stack -- o(n) skewed/worst  o(log n) balanced/best case
+        // in case of skewed since we start at root and return as soon as imbalance is found, our code actually breaks at o(n) but still the tc would remain o(n^2) in case of messed up skewed+balanced tree combination
 
-        return solve(root);
+        //bottom up -
+        // start at child nodes and go up to root
+        // tc = sc = o(n)
+
+        return checkHeight(root) != -1;
+    }
+
+    //bottom up
+    private int checkHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = checkHeight(node.left);
+        if (left == -1) return -1; // left subtree unbalanced
+
+        int right = checkHeight(node.right);
+        if (right == -1) return -1; // right subtree unbalanced
+
+        if (Math.abs(left - right) > 1) return -1; // current node unbalanced
+
+        return 1 + Math.max(left, right); // return height
     }
 
     public boolean solve(TreeNode root) {
